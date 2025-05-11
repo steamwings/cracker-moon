75dff88ad742c00ba9432d621df0cdf2e0d44d01
Zander <steamwings@outlook.com>
2025-05-11 18:50:35 -0400

    Obey locked property; remove demo controls

--- src/components/CrackerMoon.tsx
+++ src/components/CrackerMoon.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import Editor from '@hackernoon/rich-markdown-editor'
 
+import snApi from 'sn-extension-api';
+
 const CrackerMoon = () => {
   return (
     <Editor
-      autoFocus={true}
-      dark={true} // TODO support at least light and dark
-      // TODO embeds={[
+      autoFocus
+      defaultValue={snApi.text}
+      // TODO Use embeds={[
       placeholder="get crackin'!"
-      readOnly={false}
+      readOnly={snApi.locked}
       sponsored={false}
+      // TODO Use theme parameter
+      //theme={theme}
+      onChange={value => {
+        snApi.text = value();
+      }}
     />
   );
 }
+
+
 
 export default CrackerMoon
